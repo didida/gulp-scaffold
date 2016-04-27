b964b91145091079ea8989a4734a4cf1409ce6f7
momo <354512218@qq.com>
2016-04-27 22:21:34 +0800

    增加css转换rem

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var px3rem = require('gulp-px3rem');
 var sourcemaps = require('gulp-sourcemaps');
 var filter = require('gulp-filter');
 var prefix = require('gulp-autoprefixer');
@@ -16,6 +17,7 @@ gulp.task('sass', function () {
     .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
     .pipe(reload({stream: true}))
     .pipe(sourcemaps.write())
+    .pipe(px3rem({baseDpr: 1, remUnit: 10}))
     .pipe(gulp.dest(config.dest))
     .pipe(filter('**/*.css'))
     .pipe(browserSync.reload({stream: true}))
