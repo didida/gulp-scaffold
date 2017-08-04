31261a813a94f518b2c724a6052b623fa90f2549
Momo <Momo@MomodeMacBook-Pro.local>
2017-08-04 12:35:50 +0800

    babel分支

--- gulp/tasks/javascripts.js
+++ gulp/tasks/javascripts.js
@@ -1,8 +1,12 @@
 
 var gulp = require('gulp');
 var config = require('../config').js;
+var babel = require('gulp-babel')
 
 gulp.task('javascripts', function () {
   return gulp.src(config.src)
+      .pipe(babel({
+        plugins: ['transform-runtime']
+      }))
       .pipe(gulp.dest(config.dest))
 });
